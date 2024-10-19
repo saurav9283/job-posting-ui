e06d2f55cb293936333d90e950beb42b262c0a36
Saurav <sauravkumar00108@gmail.com>
2024-10-19 16:04:57 +0530

    Refactor Navbar component, add logout functionality

--- src/components/Jobs/Navbar.js
+++ src/components/Jobs/Navbar.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import logo from '../image/logo.png';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        console.log('Logout clicked');
+        localStorage.removeItem("id");
+        localStorage.removeItem("token");
+        navigate("/")
+
+    };
+
     return (
         <div className="flex justify-between items-center p-4 bg-white shadow-md">
             <div>
                 <img src={logo} alt="Logo" className="h-7" />
             </div>
-            <div className="text-lg font-semibold">Contact</div>
+            <div className=' flex gap-5'>
+                <div className="text-lg font-semibold">Contact</div>
+                <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleLogout}
+                className="ml-auto"
+            >
+                Logout
+            </Button>            </div>
         </div>
     );
 }
